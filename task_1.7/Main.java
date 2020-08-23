ffb46dbd54a8152f85016e84e65e5aa81b457e89
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-23 18:02:53 +0300

    Update Main.java

--- task_1.7/Main.java
+++ task_1.7/Main.java
@@ -11,6 +11,13 @@
 
 public class Main {
     public static void main(String[] args) {
-        //Ваш код
+       System.out.println(getVolume(25, 5, 2));
+    }
+
+    public static long getVolume(int a, int b, int c) {
+        a = 25;
+        b = 5;
+        c = 2;
+        return a * b * c*1000; //Ваш код
     }
 }
