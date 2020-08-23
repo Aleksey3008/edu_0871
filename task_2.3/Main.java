766966b6f20dd6fc2c31138e7c57266a78f66759
Aleksey3008 <69968009+Aleksey3008@users.noreply.github.com>
2020-08-23 19:05:12 +0300

    Update Main.java

--- task_2.3/Main.java
+++ task_2.3/Main.java
@@ -12,7 +12,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        //напишите тут ваш код
-
+        int a,p=1;
+            for (a = 1; a <= 10; a++) {
+                p = p * a;
+                System.out.println(p);
     }
 }
